70b9a733d861e69ca80b79f09d5c4232d906509f
Vishesh Paliwal <vishesh.23bcs10013@ms.sst.scaler.com>
2024-07-03 10:07:29 +0530

    post req

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+let courses = [
+    {id:1 , name: "java"},
+    {id:2 , name:"javascript"},
+    {id:3 , name:"python"}
+]
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
+})
+
+app.get('/course', (req, res) => {
+    res.json(courses);
+  })
+  
+app.post('/course',(req,res)=>{
+    console.log(req.body);
+    courses.push(req.body);
+    res.send("updated");
 })
